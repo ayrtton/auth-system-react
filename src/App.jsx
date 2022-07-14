5674091203dbab8885798845d24271742259c8ca
Ayrton <ayrtton.vinicius@gmail.com>
2022-07-14 12:03:22 -0300

    Build the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
+import LoginPage from './pages/jsx/LoginPage'
 
 function App() {
     return (
         <>
             <LoginPage />
-            <HomePage />
         </>
     )
 }
---
+++ src/pages/jsx/LoginPage.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import styles from '../css/LoginPage.module.css'
+
+const LoginPage = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log("Submit", {email, password})
+    }
+
+    return (
+        <div className={styles.container}>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <div className={styles.title} id="title">Sign In</div>
+                <input className={styles.textInput} type="email" name="email" id="email" placeholder="Email" required
+                    value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input className={styles.textInput} type="password" name="password" id="password" placeholder="Password" required 
+                    value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input className={styles.button} type="submit" value="Sign In" />
+            </form>
+        </div>
+    )
+}
+
+export default LoginPage
