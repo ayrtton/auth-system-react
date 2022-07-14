634751389e15c347ebd658ebede3767f3d5a280d
Ayrton <ayrtton.vinicius@gmail.com>
2022-07-14 09:37:52 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+    return (
+        <>
+            <LoginPage />
+            <HomePage />
+        </>
+    )
+}
+
+export default App
