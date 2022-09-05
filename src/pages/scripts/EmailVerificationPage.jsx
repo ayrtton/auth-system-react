6db23af1d5e5056fa34e8765eea3b8130b233b0a
Ayrton <ayrtton.vinicius@gmail.com>
2022-09-05 12:16:09 -0300

    Fix icon and logout error

--- src/pages/scripts/EmailVerificationPage.jsx
+++ src/pages/scripts/EmailVerificationPage.jsx
@@ -3,17 +3,21 @@ import { AuthContext } from "../../contexts/auth"
 import styles from "../styles/Auth.module.css"
 
 const EmailVerificationPage = () => {
-    const { resendVerificationMail } = useContext(AuthContext)
-    const [ isSent, setIsSent] = useState(false)
+    const { resendVerificationMail, logout } = useContext(AuthContext)
+    const [isSent, setIsSent] = useState(false)
 
     useEffect(() => {
         document.title = "Email Verification"
     }, [])
 
-    const handleClick = () => {
+    const handleEmailSending = () => {
         resendVerificationMail()
 
         setIsSent(true)
+    }
+
+    const handleLogout = () => {
+        logout()
     }
 
     return (
@@ -22,18 +26,18 @@ const EmailVerificationPage = () => {
                 <div style={{ display: isSent ? "none" : "block" }}>
                     Thanks for signing up!
                     <p>
-                        Before getting started, could you verify your email 
+                        Before getting started, could you verify your email
                         address by clicking on the link we just emailed to you?
                     </p>
-                    <p>
-                        If you didn't receive the email, we will gladly&nbsp; 
-                        <span className={styles.link} onClick={handleClick}>send you another</span>.
-                    </p>
+                    If you didn't receive the email, we will gladly&nbsp;
+                    <span className={styles.link} onClick={handleEmailSending}>send you another</span>.
                 </div>
                 <div style={{ display: isSent ? "block" : "none" }}>
                     <p className={styles.center}>Verification email successfully sent!</p>
                 </div>
-                <div class={styles.center}><a className={styles.link} href="/">Log Out</a></div>
+                <div class={styles.center}>
+                    <button className={styles.button} onClick={handleLogout}>Logout</button>
+                </div>
             </div>
         </div>
     )
