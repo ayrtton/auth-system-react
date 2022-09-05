34908fa5be4c84b015a90c33293d0da20b9ed719
Ayrton <ayrtton.vinicius@gmail.com>
2022-09-05 12:05:20 -0300

    Set the private page design

--- src/pages/scripts/PrivatePage.jsx
+++ src/pages/scripts/PrivatePage.jsx
@@ -1,18 +1,22 @@
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/auth"
+import styles from "../styles/Auth.module.css"
 
 const PrivatePage = () => {
     const { logout } = useContext(AuthContext)
+    const user = JSON.parse(localStorage.getItem("user"))
 
     const handleLogout = () => {
         logout()
     }
 
     return (
-        <>
-            <button onClick={handleLogout}>Logout</button>
-            <p>Under Construction...</p>
-        </>
+        <div className={styles.container}>
+            <div className={styles.form}>
+                <span>Welcome {user.name}! You are logged in...</span>
+                <button className={styles.button} onClick={handleLogout}>Logout</button>
+            </div>
+        </div>
     )
 }
 
